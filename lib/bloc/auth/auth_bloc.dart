dfb1876e68b3b65596154c718cd35544bbfee205
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-04 01:17:31 +0700

    menambah logika autentikasi pada metode _onLoginSubmitted

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -8,4 +8,15 @@ class AuthBloc {
     on<RegisterSubmitted>(_onRegisterSubmitted);
     on<LogoutRequested>(_onLogoutRequested);
   }
+
+  Future<void> _onLoginSubmitted(LoginSubmitted event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    await Future.delayed(const Duration(1));
+
+    if (event.email.isNotEmpty && event.password.isNotEmpty) {
+      emit(AuthAuthenticated(email: event.email));
+    } else {
+      emit(const AuthError('Email dan password salah'));
+    }
+  }
 }
