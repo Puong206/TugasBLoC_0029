cf36d2a64df901a99b7a30a373f95dfb0161e04e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-03 22:11:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:statemanagement/auth/login.dart';
+import 'package:statemanagement/mainui/home.dart';
+import 'package:statemanagement/page/order_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/order': (context) => const OrderPage(),
+      },
+    );
+  }
+}
