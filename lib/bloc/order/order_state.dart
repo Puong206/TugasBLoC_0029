f6a77d7aef735ae198afa97c6e3cd6c64e4d569c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-06 02:57:06 +0700

    menambah kelas OrderSuccess dan memperbarui metode props

--- lib/bloc/order/order_state.dart
+++ lib/bloc/order/order_state.dart
@@ -8,3 +8,15 @@ abstract class OrderState extends Equatable {
 class OrderInitial extends OrderState {}
 
 class OrderLoading extends OrderState {}
+
+class OrderSuccess extends OrderState {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+  final int totalHarga;
+
+
+  @override
+  List<Object> get props => [message];
+}
