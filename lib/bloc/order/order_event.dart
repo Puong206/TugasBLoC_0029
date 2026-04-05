eb33996e2dc2d1b939e100814c2e132fb9e11481
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-06 02:41:10 +0700

    menambah kelas OrderSubmitted pada order_event.dart

--- lib/bloc/order/order_event.dart
+++ lib/bloc/order/order_event.dart
@@ -4,3 +4,17 @@ abstract class OrderEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+class OrderSubmitted extends OrderEvent {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+
+  OrderSubmitted({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+  });
+}
