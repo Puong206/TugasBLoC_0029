c94bf73f1e52b8c8e45ed8c69b31c53d726a76cd
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-06 01:27:55 +0700

    menambah logika pemrosesan pesanan pada kelas OrderBloc

--- lib/bloc/order/order_bloc.dart
+++ lib/bloc/order/order_bloc.dart
@@ -5,5 +5,10 @@ part 'order_event.dart';
 part 'order_state.dart';
 
 class OrderBloc extends Bloc<OrderEvent, OrderState> {
+  OrderBloc() : super(OrderInitial()) {
+    on<OrderSubmitted>(_onOrderSubmitted);
+    on<OrderReset>(_onOrderReset);
+  }
+
   
 }
