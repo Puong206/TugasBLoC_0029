12b6d3c2fcf92451ec528acaee9934c5d158b62e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-06 01:26:46 +0700

    menambah kelas OrderBloc dan mengimpor dependensi yang diperlukan

---
+++ lib/bloc/order/order_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'order_event.dart';
+part 'order_state.dart';
+
+class OrderBloc extends Bloc<OrderEvent, OrderState> {
+  
+}
