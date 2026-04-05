8a79d56106442a5cc8fa5191e603ef49b86ece8b
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-06 03:09:03 +0700

    menambah kelas MyApp dan mengonfigurasi MultiBlocProvider

--- lib/mainbloc.dart
+++ lib/mainbloc.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:statemanagement/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:statemanagement/auth/login.dart';
+import 'package:statemanagement/bloc/auth/auth_bloc.dart';
+import 'package:statemanagement/bloc/order/order_bloc.dart';
+import 'package:statemanagement/mainui/home.dart';
+import 'package:statemanagement/page/order_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (_) => AuthBloc()),
+        BlocProvider<OrderBloc>(create: (_) => OrderBloc()),
+      ],
+      child: MaterialApp(
+        title: 'BloC State Management App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+        ),
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/home': (context) => HomePage(),
+          '/order': (context) => OrderPage(),
+        },
+      ),
+    );
+  }
 }
